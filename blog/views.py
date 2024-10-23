84841a932930c23487e4b25fdabcf7ece0ae4a45
joimping <jo.imping@dhbw-engineering.de>
2024-10-23 16:10:58 +0200

    mehrere tech details

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 def index (request): 
 
     # XML-Datei parsen
-    tree = ET.parse('xml/data_copy.xml')
+    tree = ET.parse('xml/data.xml')
     root = tree.getroot()
 
 
@@ -19,6 +19,18 @@ def index (request):
         technische_details = auto.find('Technische_Details')
         auto_daten['leistung'] = technische_details.find('Leistung').text
         auto_daten['drehmoment'] = technische_details.find('max_Drehmomemt').text
+        auto_daten['hubraum'] = technische_details.find('Hubraum').text
+        auto_daten['zylinderzahl'] = technische_details.find('Zylinderzahl').text
+        auto_daten['verbrauch'] = technische_details.find('Verbrauch').text
+        auto_daten['getriebe'] = technische_details.find('Getriebe').text
+        auto_daten['leergewicht'] = technische_details.find('Leergewicht').text
+        auto_daten['hoechstgeschwindigkeit'] = technische_details.find('Hoechstgeschwindigkeit').text
+        auto_daten['beschleunigung_0_100'] = technische_details.find('Beschleunigung_0_100').text
+        auto_daten['erscheinungsjahr'] = technische_details.find('Erscheinungsjahr').text
+        auto_daten['anzahl_tueren'] = technische_details.find('Anzahl_tueren').text
+        auto_daten['farbe'] = technische_details.find('Farbe').text  # neues Attribut
+        auto_daten['preis'] = technische_details.find('Preis').text    # neues Attribut
+        auto_daten['baujahr'] = technische_details.find('Baujahr').text  # neues Attribut
         auto_daten['image_url'] = technische_details.find('image_url').text
         blog_daten.append(auto_daten)
 
